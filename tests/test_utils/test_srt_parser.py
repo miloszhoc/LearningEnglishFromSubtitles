fa37f88e2650f3033b3abe197603bd6380e8a7fc
milosz.hoc <miloszhoc@gmail.com>
2019-06-25 22:39:08 +0200

    fixing imports/updating folders structure

--- tests/test_utils/test_srt_parser.py
+++ tests/test_utils/test_srt_parser.py
@@ -1,13 +1,17 @@
 import unittest
-from utils.srt_parser import SrtParser
+from utils import srt_parser
 
 
 class TestSrtParser(unittest.TestCase):
 
     def test_read_srt_file_when_file_exists(self):
-        parser = SrtParser(file='part_to_test.srt')
+        parser = srt_parser.SrtParser(file='part_to_test.srt')
         self.assertTrue(parser.read_srt_file())
 
     def test_read_srt_file_when_file_doesnt_exists(self):
-        parser = SrtParser(file='part.srt')
+        parser = srt_parser.SrtParser(file='part.srt')
         self.assertFalse(parser.get_words_from_file())
+
+
+if __name__ == '__main__':
+    unittest.main()
