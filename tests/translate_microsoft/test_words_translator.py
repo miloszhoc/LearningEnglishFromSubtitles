81a3feb88fc2ceb2985d6e3181d9ac8d06153197
milosz.hoc <miloszhoc@gmail.com>
2019-07-22 18:21:20 +0200

    deleted unused test cases

--- tests/translate_microsoft/test_words_translator.py
+++ tests/translate_microsoft/test_words_translator.py
@@ -12,7 +12,7 @@ class TestTranslateMicrosoft(unittest.TestCase):
                           words_list=words_list,
                           api_key=api_key,
                           src_lang='abc',
-                          dest_lang='pl'
+                          dest_lang='en'
                           )
 
     def test_init_when_dest_lang_does_not_exists(self):
@@ -24,22 +24,13 @@ class TestTranslateMicrosoft(unittest.TestCase):
                           dest_lang='pla'
                           )
 
-    def test_init_when_src_lang_and_dest_lang_does_not_exists(self):
-        self.assertRaises(ValueError,
-                          words_translator.TranslateWordsMicrosoft,
-                          words_list=words_list,
-                          api_key=api_key,
-                          src_lang='abc',
-                          dest_lang='pla'
-                          )
-
     def test_init_when_src_lang_is_readable_and_both_lang_exists(self):
         tr = words_translator.TranslateWordsMicrosoft(words_list=words_list,
                                                       api_key=api_key,
-                                                      src_lang='English',
-                                                      dest_lang='pl')
+                                                      src_lang='Polish',
+                                                      dest_lang='en')
 
-        self.assertEqual(tr.src_lang, 'en')
+        self.assertEqual(tr.src_lang, 'pl')
 
     def test_init_when_dest_lang_is_readable_and_both_lang_exists(self):
         tr = words_translator.TranslateWordsMicrosoft(words_list=words_list,
@@ -49,45 +40,12 @@ class TestTranslateMicrosoft(unittest.TestCase):
 
         self.assertEqual(tr.dest_lang, 'pl')
 
-    def test_init_when_both_are_readable_and_both_lang_exists(self):
-        tr = words_translator.TranslateWordsMicrosoft(words_list=words_list,
-                                                      api_key=api_key,
-                                                      src_lang='english',
-                                                      dest_lang='polish')
-
-        self.assertEqual(tr.dest_lang, 'pl')
-
-    def test_init_if_dest_lang_is_english_and_src_lang_does_not_exist(self):
-        self.assertRaises(ValueError, words_translator.TranslateWordsMicrosoft,
-                          words_list=words_list,
-                          api_key=api_key,
-                          src_lang='xx',
-                          dest_lang='english')
-
-    def test_init_if_dest_lang_does_not_exist_and_src_lang_is_english(self):
-        self.assertRaises(ValueError, words_translator.TranslateWordsMicrosoft,
-                          words_list=words_list,
-                          api_key=api_key,
-                          src_lang='english',
-                          dest_lang='zcvb')
-
-    def test_init_if_both_languages_are_different_than_english(self):
-        tr = words_translator.TranslateWordsMicrosoft(words_list=words_list,
-                                                      api_key=api_key,
-                                                      src_lang='pl',
-                                                      dest_lang='de')
-        self.assertEqual(tr.dest_lang, 'en')
-        self.assertEqual(tr.src_lang, 'pl')
-
     def test_init_if_src_lang_is_english_and_dest_lang_exist(self):
         tr = words_translator.TranslateWordsMicrosoft(words_list=words_list,
                                                       api_key=api_key,
                                                       src_lang='en',
                                                       dest_lang='de')
         self.assertEqual(tr.dest_lang, 'de')
-
-    def test_show_all_languages(self):
-        self.assertGreater(len(words_translator.TranslateWordsMicrosoft.show_all_languages()), 0)
 
     def test_translate_words(self):
         tr = words_translator.TranslateWordsMicrosoft(words_list=words_list,
