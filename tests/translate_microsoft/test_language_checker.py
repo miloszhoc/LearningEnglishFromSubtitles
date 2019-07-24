cafd14a72a9be7bbe83d7106d0ca7a1de3f549b7
milosz.hoc <miloszhoc@gmail.com>
2019-07-24 23:19:10 +0200

    updated test cases

--- tests/translate_microsoft/test_language_checker.py
+++ tests/translate_microsoft/test_language_checker.py
@@ -4,12 +4,35 @@ from translate_microsoft import language_checker
 
 
 class TestLanguageChecker(unittest.TestCase):
+
     def test_show_all_languages_dictionary(self):
         lc = language_checker.CheckLanguage()
         self.assertGreater(len(lc.show_all_languages_dictionary()), 0)
 
-    def test_show_all_languages_transaltion(self):
+    def test_check_lang_dictionary_when_lang_exists_lower_case(self):
+        lc = language_checker.CheckLanguage()
+        self.assertEqual(lc.check_lang_dictionary('greek'), 'el')
+
+    def test_check_lang_dictionary_when_lang_exists_capitalize(self):
+        lc = language_checker.CheckLanguage()
+        self.assertEqual(lc.check_lang_dictionary('German'), 'de')
+
+    def test_check_lang_dictionary_when_lang_exists_upper_case(self):
+        lc = language_checker.CheckLanguage()
+        self.assertEqual(lc.check_lang_dictionary('FRENCH'), 'fr')
+
+    def test_check_lang_dictionary_when_lang_does_not_exists(self):
+        lc = language_checker.CheckLanguage()
+        self.assertRaises(ValueError, lc.check_lang_dictionary, 'xda')
+
+    def test_show_all_languages_translation(self):
         lc = language_checker.CheckLanguage()
         self.assertGreater(len(lc.show_all_languages_translation()), 0)
 
-    # todo: finish tests
+    def test_check_lang_translation_when_lang_exists(self):
+        lc = language_checker.CheckLanguage()
+        self.assertEqual(lc.check_lang_translation('Tongan'), 'to')
+
+    def test_check_lang_translation_when_lang_does_not_exists(self):
+        lc = language_checker.CheckLanguage()
+        self.assertRaises(ValueError, lc.check_lang_translation, 'qwerty')
