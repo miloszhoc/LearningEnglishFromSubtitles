62c76791a6e595424fce7a7c8fc30d6e7074f46c
milosz.hoc <miloszhoc@gmail.com>
2019-07-26 19:52:23 +0200

    test cases updated

--- tests/translate_microsoft/test_srt_parser.py
+++ tests/translate_microsoft/test_srt_parser.py
@@ -95,20 +95,35 @@ class TestSrtParser(unittest.TestCase):
         words = parser.words_with_frequency(min_len=3, min_occurs=2, descending=False)
         self.assertListEqual(words, [('white', '2')])
 
-    def test_part_subtitles_when_num_in_srt_file(self):
+    def test_part_subtitles_when_num_is_passed(self):
         parser = srt_parser.SrtParser(file=path_to_file)
-        text = parser.part_subtitles('296')
+        text = parser.part_subtitles(group_num='296')
         self.assertListEqual(text, ['...than we could spend', 'in 10 lifetimes.'])
 
     def test_part_subtitles_when_num_not_in_srt_file(self):
         parser = srt_parser.SrtParser(file=path_to_file)
-        text = parser.part_subtitles('58')
+        text = parser.part_subtitles(group_num='58')
         self.assertFalse(text)
 
     def test_part_subtitles_when_num_is_not_number(self):
         parser = srt_parser.SrtParser(file=path_to_file)
-        text = parser.part_subtitles('x')
+        text = parser.part_subtitles(group_num='x')
         self.assertFalse(text)
+
+    def test_part_subtitles_when_time_is_passed(self):
+        parser = srt_parser.SrtParser(file=path_to_file)
+        text = parser.part_subtitles(time='00:01:17')
+        self.assertListEqual(text, ["Mr. White,", "the car's been dealt with, sir."])
+
+    def test_part_subtitles_when_not_existing_time_is_passed(self):
+        parser = srt_parser.SrtParser(file=path_to_file)
+        text = parser.part_subtitles(time='00:11:17')
+        self.assertFalse(text)
+
+    def test_part_subtitles_when_wrong_time_format_is_passed(self):
+        parser = srt_parser.SrtParser(file=path_to_file)
+        part = parser.part_subtitles
+        self.assertRaises(ValueError, part, time='00:1211:17')
 
 
 if __name__ == '__main__':
